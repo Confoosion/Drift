74549680b5847a587dc6fc59f72efb641d4af106
Jake Sales <jfsales@ucsc.edu>
2024-06-07 18:14:52 -0700

    Added hitboxes
    Car won't go out of bounds (outside the track).

---
+++ assets/wallTileset.tsx
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="wallTileset" tilewidth="32" tileheight="32" tilecount="5184" columns="72">
+ <image source="spritesheet_tiles.png" width="2304" height="2304"/>
+ <tile id="1164">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1384">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1455">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1675">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2611">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2683">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2755">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2827">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2900">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2901">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2902">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2903">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3108">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3109">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3110">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3111">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3184">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3256">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3328">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3400">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4327">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4399">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4471">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4543">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4900">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4972">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5044">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5116">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
--- src/Scenes/Game.js
+++ src/Scenes/Game.js
@@ -18,14 +18,20 @@ class Game extends Phaser.Scene {
         this.map = this.add.tilemap("tutorial_track", this.TILESIZE, this.TILESIZE, this.TILEHEIGHT, this.TILEWIDTH);
 
         this.tileset = this.map.addTilesetImage("trackTileset", "track_tiles");
+        this.wall = this.map.addTilesetImage("wallTileset", "track_tiles");
 
         this.backgroundLayer = this.map.createLayer("backgroundLayer", this.tileset, 0, this.TILEHEIGHTOFFSET);
         this.trackLayer = this.map.createLayer("trackLayer", this.tileset, 0, this.TILEHEIGHTOFFSET);
-
+        this.wallLayer = this.map.createLayer("wallLayer", this.wall, 0, 0);
+        this.wallLayer.setCollisionByProperty({
+            collides: true
+        });
 
         my.sprite.player = this.physics.add.sprite(300, 500, "yellowCar").setScale(0.75); 
-        // this.car1 = this.add.sprite(300, 500, "yellowCar");
-        // this.car1.setScale(0.75);
+        const radius = my.sprite.player.height * 0.25;
+        my.sprite.player.body.setCircle(radius, 4, radius);
+
+        this.physics.add.collider(my.sprite.player, this.wallLayer);
 
         cursors = this.input.keyboard.createCursorKeys();
     }
@@ -43,7 +49,7 @@ class Game extends Phaser.Scene {
                 this.velocity -= 7;
             }
         }
-
+        let angle = 0;
         if(cursors.left.isDown)
         {
             my.sprite.player.body.angularVelocity = -5*(this.velocity/10);
@@ -55,9 +61,13 @@ class Game extends Phaser.Scene {
         else
         {
             my.sprite.player.body.angularVelocity = 0;
-        }
+        } 
 
+        // my.sprite.player.angle = my.sprite.player.body.angularVelocity;
         my.sprite.player.body.velocity.x = this.velocity * Math.cos((my.sprite.player.angle-90)*0.01745);
         my.sprite.player.body.velocity.y = this.velocity * Math.sin((my.sprite.player.angle-90)*0.01745);
+        // this.physics.velocityFromRotation(my.sprite.player.rotation, this.velocity, my.sprite.player.body.velocity);
+        // this.car1.body.velocity.x = this.velocity * Math.cos((this.car1.angle-90)*0.01745);
+        // this.car1.body.velocity.y = this.velocity * Math.sin((this.car1.angle-90)*0.01745);
     }
 }
